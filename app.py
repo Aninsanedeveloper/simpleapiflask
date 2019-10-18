a37950d151a61e9b2b95e6248b93279fde3cfd75
Marino Salazar <marinosalazar@gmail.com>
2019-10-17 21:31:21 -0400

    some changes on code, practicing how to build a api on flask

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
-from flask import Flask , jsonify
+from flask import Flask , jsonify , render_template , request
 from products import products
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Home page'
+    return render_template('home.html')
 
 @app.route('/products')
 def getProducts():
     return jsonify(products)
 
+@app.route('/products' , methods =['POST'])
+def postProducts():
+    newItem = {
+        "name" : request.json['name'],
+        "price": request.json['price'],
+        "marca": request.json['marca']
+    }
+    products.append(newItem)
+    return jsonify({"message": "NEW LIST", "products": products})
+
 @app.route('/products/<string:product_name>')
 def getProductName(product_name):
      productFound =  [p for p in products if p['name'] == product_name ]
-     return jsonify(productFound[0])
+     if (len(productFound) > 0):
+         return jsonify({"the product that we found is ": productFound[0]})
+     return jsonify({"message": "Ohhh product doesnt exist! "})
+
+@app.route('/products/<string:product_name>', methods=['PUT'])
+def fixProduct(product_name):
+    item = [ i for i in products if product_name == i['name']]
+    if (len(item) > 0):
+        item[0]['name'] = request.json['name']
+        item[0]['price'] = request.json['price']
+        item[0]['marca'] = request.json['marca']
+        return jsonify(item)
+    return jsonify({"message": "items match not found", "products": products})
+     
    
 
 if __name__ == '__main__':
